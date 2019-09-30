2115fa057c1deb1ed60237d4689e5758b703b104
Jared Madison <jaredmadison@icloud.com>
2019-09-30 17:25:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,46 @@
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo" ]
+  next_color = "violet"
+    colors_in_the_rainbow.using_push(Violet)
+    
+  
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  @new_neighborhoods = "Staten Island"
+    @new_neighborhoods.using_unshift(Staten Island)
+    
+@continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antartica" ]
+    Antartica = @continents.using_pop
+    
+@dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  (Chihuahua,Shiba Inu) = @dog_breeds.pop_with_args
+  
+@my_favorite_cities = ["Lagos", "Cape Town", " Nairobi", "San Francisco", "Gabrone", "New York", "Berlin", " London"]
+  @im_so_over_this_city.using_unshift = ("Lagos")
+  
+@ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  shift_with_args(@ice_cream_brands)
+  
+@my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+@all_of_my_favs = (@my_favorite_things, more_favs)
+  @all_of_my_favs.using_concat
+  
+@list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "C++" "PHP"]
+  @another_language = "Phython"
+  @list_of_programming_languages.using_insert
+  
+haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  haircuts.using_uniq
+  
+instruments = ["Saxophone", ["Piano", "Trumpet"] "Violin", "Drums", "Flute"]
+  instuments.using_flatten
+  
+
+
+  
+
+      
+
+      
+
+    
+    
+  
